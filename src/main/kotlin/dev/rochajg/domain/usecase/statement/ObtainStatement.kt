4a6d022d0baa1002f76a8b0ba5bbed160c6ea3e4
Jordan Rocha <70rd23@gmail.com>
2024-02-10 01:08:26 -0300

    feat: create use case to obtain user statement

---
+++ src/main/kotlin/dev/rochajg/domain/usecase/statement/ObtainStatement.kt
@@ -0,0 +1,39 @@
+package dev.rochajg.domain.usecase.statement
+
+import dev.rochajg.domain.entity.user.UserBalance
+import dev.rochajg.domain.entity.user.UserTransaction
+import dev.rochajg.domain.entity.user.UserTransactions
+import dev.rochajg.domain.gateway.transaction.TransactionGateway
+import dev.rochajg.domain.gateway.user.UserGateway
+import jakarta.inject.Singleton
+import java.util.Date
+
+@Singleton
+class ObtainStatement(
+    private val transactionGateway: TransactionGateway,
+    private val userGateway: UserGateway,
+) {
+    fun getStatement(userId: Int): UserTransaction {
+        val user = userGateway.get(userId) ?: throw Exception("User not found")
+        val userTransaction = transactionGateway.getByUser(userId)
+
+        return UserTransaction(
+            saldo =
+                UserBalance(
+                    total = user.balance,
+                    dataExtrato = Date(),
+                    limite = user.limit,
+                ),
+            ultimasTransacoes =
+                userTransaction
+                    .map {
+                        UserTransactions(
+                            valor = it.value,
+                            tipo = it.operation,
+                            descricao = it.description,
+                            realizadaEm = it.createdAt,
+                        )
+                    },
+        )
+    }
+}
