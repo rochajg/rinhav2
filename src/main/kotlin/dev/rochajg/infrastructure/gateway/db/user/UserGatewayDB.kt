35530c448e1f470c79bf7306210a5452ac029f52
Jordan Rocha <70rd23@gmail.com>
2024-02-10 01:06:08 -0300

    feat: configure user gateway for database

---
+++ src/main/kotlin/dev/rochajg/infrastructure/gateway/db/user/UserGatewayDB.kt
@@ -0,0 +1,19 @@
+package dev.rochajg.infrastructure.gateway.db.user
+
+import dev.rochajg.domain.entity.user.User
+import dev.rochajg.domain.gateway.user.UserGateway
+import dev.rochajg.infrastructure.db.repository.user.UserRepository
+import dev.rochajg.infrastructure.db.schema.user.toEntity
+import dev.rochajg.infrastructure.db.schema.user.toSchema
+import jakarta.inject.Singleton
+
+@Singleton
+class UserGatewayDB(
+    private val userRepository: UserRepository,
+) : UserGateway {
+    override fun get(id: Int): User? = userRepository.get(id)?.toEntity()
+
+    override fun update(user: User) {
+        userRepository.update(user.toSchema())
+    }
+}
