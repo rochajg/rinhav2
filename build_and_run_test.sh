e6ac0a5187f363164043298227dd62af388b8fa8
Jordan Rocha <70rd23@gmail.com>
2024-02-11 20:02:24 -0300

    build: add build and run script

---
+++ build_and_run_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Executar o comando gradle para limpar e construir a imagem do Docker
+./gradlew clean build
+
+# Construir a imagem
+docker build -t rochajg/rinhav2:latest .
+
+# Listar as imagens Docker disponíveis
+docker image list rochajg/rinhav2:latest
+
+# Remover os containers existentes e limpar a base de dados do MongoDB
+docker-compose down -v
+
+# Iniciar os contêineres utilizando o docker-compose
+docker-compose up -d
+
+# Adiciona sleep para aguardar os contêineres iniciarem
+echo "Iniciando containers..."
+sleep 30
+
+# Executar o script de teste de carga
+./load-test/run-test.sh
