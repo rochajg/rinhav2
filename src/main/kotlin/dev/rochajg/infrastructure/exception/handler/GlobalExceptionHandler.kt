0c9bf54648ecae50d892dca5628420a23985623e
Jordan Rocha <70rd23@gmail.com>
2024-02-11 18:15:07 -0300

    feat: add request and response information on error logs

--- src/main/kotlin/dev/rochajg/infrastructure/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/dev/rochajg/infrastructure/exception/handler/GlobalExceptionHandler.kt
@@ -1,6 +1,8 @@
 package dev.rochajg.infrastructure.exception.handler
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import dev.rochajg.application.exception.ApiException
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
@@ -11,48 +13,73 @@ import mu.KotlinLogging
 import org.slf4j.Logger
 
 @Controller
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(
+    private val objectMapper: ObjectMapper,
+) {
     private val logger: Logger = KotlinLogging.logger {}
 
     @Error(global = true, exception = Exception::class)
-    fun genericErrorHandler(exception: Exception): HttpResponse<ApiError> =
+    fun genericErrorHandler(
+        request: HttpRequest<*>,
+        exception: Exception,
+    ): HttpResponse<ApiError> =
         HttpResponse.serverError(
             ApiError(
                 error = "internal_error",
                 message = "Internal Server Error",
             ),
-        ).also { logError(exception) }
+        ).also { logError(request, it, exception) }
 
     @Error(global = true, exception = ValidationException::class)
-    fun validationErrorHandler(validationException: ValidationException): HttpResponse<ApiError> =
+    fun validationErrorHandler(
+        request: HttpRequest<*>,
+        validationException: ValidationException,
+    ): HttpResponse<ApiError> =
         HttpResponse.badRequest(
             ApiError(
                 error = "validation_error",
                 message = validationException.message,
             ),
-        ).also { logError(validationException) }
+        ).also { logError(request, it, validationException) }
 
     @Error(global = true, exception = UnsatisfiedRouteException::class)
-    fun unsatisfiedRouteErrorHandler(unsatisfiedHeaderException: UnsatisfiedRouteException): HttpResponse<ApiError> =
+    fun unsatisfiedRouteErrorHandler(
+        request: HttpRequest<*>,
+        unsatisfiedHeaderException: UnsatisfiedRouteException,
+    ): HttpResponse<ApiError> =
         HttpResponse.badRequest(
             ApiError(
                 error = "validation_error",
                 message = unsatisfiedHeaderException.message,
             ),
-        ).also { logError(unsatisfiedHeaderException) }
+        ).also { logError(request, it, unsatisfiedHeaderException) }
 
     @Error(global = true, exception = ApiException::class)
-    fun apiExceptionHandler(apiException: ApiException): HttpResponse<ApiError> =
+    fun apiExceptionHandler(
+        request: HttpRequest<*>,
+        apiException: ApiException,
+    ): HttpResponse<ApiError> =
         HttpResponse.status<ApiError>(HttpStatus.valueOf(apiException.statusCode))
             .body(
                 ApiError(
-                    apiException.error,
+                    error = apiException.error,
                     message = apiException.message,
                     cause = apiException.cause?.message,
                 ),
-            ).also { logError(apiException) }
+            ).also { logError(request, it, apiException) }
 
-    fun logError(error: Throwable) {
-        logger.error(error.message, error)
+    private fun logError(
+        request: HttpRequest<*>,
+        response: HttpResponse<ApiError>,
+        error: Throwable,
+    ) {
+        String()
+            .plus("[request_method: ${request.method}]\n")
+            .plus("[request_uri: ${request.path}]\n")
+            .plus("[request_body: ${objectMapper.writeValueAsString(request.body)}]\n")
+            .plus("[response_status: ${response.status.code}]\n")
+            .plus("[response_body: ${objectMapper.writeValueAsString(response.body())}]\n")
+            .plus("[error: ${error.message}]")
+            .also { logger.error(it, error) }
     }
 }
