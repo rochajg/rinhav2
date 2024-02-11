9726552eb03426bb47157630c45d8558700ec149
Jordan Rocha <70rd23@gmail.com>
2024-02-11 19:59:12 -0300

    refactor: validate transaction type on application request package

--- src/main/kotlin/dev/rochajg/application/entrypoint/client/entity/CreateTransactionRequest.kt
+++ src/main/kotlin/dev/rochajg/application/entrypoint/client/entity/CreateTransactionRequest.kt
@@ -1,6 +1,7 @@
 package dev.rochajg.application.entrypoint.client.entity
 
 import dev.rochajg.domain.usecase.transaction.entity.TransactionRequest
+import dev.rochajg.domain.usecase.transaction.entity.TransactionType
 import dev.rochajg.domain.usecase.transaction.exception.InvalidTransactionRequestException
 
 data class CreateTransactionRequest(
@@ -19,11 +20,13 @@ fun CreateTransactionRequest.toDomain(): TransactionRequest {
             throw InvalidTransactionRequestException("Description must not be blank")
         this.descricao.length !in 0..10 ->
             throw InvalidTransactionRequestException("Description must be between 0 and 10 characters")
+        this.tipo !in listOf("c", "d") ->
+            throw InvalidTransactionRequestException("Invalid transaction type")
     }
 
     return TransactionRequest(
         value = valor.toInt(),
-        type = tipo,
+        type = TransactionType.valueOf(tipo.uppercase()),
         description = descricao!!,
     )
 }
