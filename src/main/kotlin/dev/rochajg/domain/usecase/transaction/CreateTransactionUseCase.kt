a9bff50ce3713c5cd056ef9ad5b3f7ab6035d162
Jordan Rocha <70rd23@gmail.com>
2024-02-11 18:08:33 -0300

    fix: adjust balance validation when creating a transaction

--- src/main/kotlin/dev/rochajg/domain/usecase/transaction/CreateTransactionUseCase.kt
+++ src/main/kotlin/dev/rochajg/domain/usecase/transaction/CreateTransactionUseCase.kt
@@ -3,10 +3,11 @@ package dev.rochajg.domain.usecase.transaction
 import dev.rochajg.domain.entity.transaction.Transaction
 import dev.rochajg.domain.gateway.transaction.TransactionGateway
 import dev.rochajg.domain.gateway.user.UserGateway
+import dev.rochajg.domain.usecase.exception.UserNotFoundException
 import dev.rochajg.domain.usecase.transaction.entity.CreatedTransaction
 import dev.rochajg.domain.usecase.transaction.entity.TransactionRequest
 import dev.rochajg.domain.usecase.transaction.exception.InsufficientBalanceException
-import dev.rochajg.domain.usecase.transaction.exception.UserNotFoundException
+import dev.rochajg.domain.usecase.transaction.exception.InvalidTransactionRequestException
 import jakarta.inject.Singleton
 
 @Singleton
@@ -15,25 +16,25 @@ class CreateTransactionUseCase(
     private val transactionGateway: TransactionGateway,
 ) {
     fun createTransaction(
-        userId: Int,
+        userId: Int?,
         transactionRequest: TransactionRequest,
     ): CreatedTransaction {
         val user = userGateway.get(userId) ?: throw UserNotFoundException(userId)
-        var newBalance = user.balance
 
-        if (transactionRequest.type == "c") {
-            newBalance += transactionRequest.value
-        } else {
-            if (user.balance - transactionRequest.value < -user.limit) {
-                throw InsufficientBalanceException(userId)
-            }
-
-            newBalance -= transactionRequest.value
+        if (kotlin.math.abs(transactionRequest.value - user.balance) > user.limit) {
+            throw InsufficientBalanceException(user.id, user.balance)
         }
+
+        val newBalance =
+            when (transactionRequest.type) {
+                "c" -> user.balance + transactionRequest.value
+                "d" -> user.balance - transactionRequest.value
+                else -> throw InvalidTransactionRequestException("Invalid transaction type")
+            }
 
         transactionGateway.create(
             Transaction(
-                userId = userId,
+                userId = user.id,
                 value = transactionRequest.value,
                 operation = transactionRequest.type,
                 description = transactionRequest.description,
